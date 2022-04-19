fae23f9c10b1465bceaac1ad4b5699d8dd6654d4
杨阳 <1002522146@qq.com>
2022-04-19 19:29:37 +0800

    :sparkles: 提交一般

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { World, Cube, useAnimation, Model, ThirdPersonCamera, Skybox, useKeyboard, useLoop, usePreload } from "lingo3d-react"
+import { useRef } from 'react'
+
+const Game = () => {
+  const key = useKeyboard()
+  const characterRef = useRef()
+  useLoop(() => {
+    characterRef.current.moveForward(-10)
+  }, key === 'w')
+  const movtion = key === 'w' ? "walking" : "idle"
+  return (
+    <World>
+      <Model src="Grassland.glb" scale={270} physics="map-debug" />
+      <ThirdPersonCamera active mouseControl>
+        <Model
+          ref={characterRef}
+          src="Fox.fbx"
+          physics="character"
+          animations={{ idle: "Rifle Idle.fbx", walking: "Rifle Run.fbx" }}
+          animation={movtion}
+          visible={false}
+        />
+      </ThirdPersonCamera>
+      <Skybox texture='skybox.jpg' />
+    </World>
+  )
+}
+const App = () => {
+  const progress = usePreload([
+    "Fox.fbx",
+    "Grassland.glb",
+    "ground.jpeg",
+    "gun.glb",
+    "Idle.fbx",
+    "Rifle Idle.fbx",
+    "Rifle Run.fbx",
+    "skybox.jpg",
+    "Walking.fbx"
+  ], "6.6mb")
+
+  if (progress < 100)
+    return (
+      <div style={{
+        width: "100vw",
+        height: "100vh",
+        left: 0,
+        top: 0,
+        backgroundColor: "black",
+        color: "white",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+      }}>
+        loading {Math.round(progress)}%
+      </div>
+    )
+
+  return (
+    <Game />
+  )
+}
+
+export default App
